cd80597575d6a582a3e7ec5fc1d2d7eaf95adc63
Eric Peter <75148956+ericmpeter@users.noreply.github.com>
2021-01-22 13:04:23 -0700

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -8,11 +8,17 @@ stops = set(stop_words.ENGLISH_STOP_WORDS)
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
+    
+    #change everything to lowercase
+    line = line.strip().lower()
+    
+    #remove punctuation
+    line.translate(None, string.punctuation)
 
     # split the line into words; splits on any whitespace
     words = line.split()
-
-    # output tuples (word, 1) in tab-delimited format
+    
+    # remove stopwords, output tuples (word, 1) in tab-delimited format
     for word in words:
         if word not in stops:
             print '%s\t%s' % (word, "1")
